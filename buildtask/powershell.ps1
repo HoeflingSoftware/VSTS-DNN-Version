98f79b9c6daebc649eca551e7baf8f67a1b17c72
Andrew Hoefling <andrew@hoeflingsoftware.com>
2017-09-21 19:39:23 -0400

    Added recursive search and update so the script now finds all *.dnn files and updates the version information - fixes #1

--- buildtask/powershell.ps1
+++ buildtask/powershell.ps1
@@ -1,23 +1,33 @@
 [CmdletBinding()]
 param()
 
-Trace-VstsEnteringInvocation $MyInvocation
+#Trace-VstsEnteringInvocation $MyInvocation
 try {
     # Get inputs.
     $input_source = Get-VstsInput -Name 'sourceDirectory' -Require
     $input_version = Get-VstsInput -Name 'moduleVersion' -Require
 
-    # load XML file
-    $xml = New-Object XML
-    $xml.Load($input_source)
+    # get all dnn manifest files based on the source
+    Write-Host "Finding all *.dnn manifest files recursively from $input_source"
+    $manifestFiles = Get-ChildItem $input_source -name -recurse *.dnn
 
-    # select node and update version
-    $nodes = $xml.SelectNodes("/dotnetnuke/packages/package")
-    $nodes.SetAttribute("version", $input_version)
+    Foreach ($manifest in $manifestFiles)
+    {
+        $path = "$input_source\$manifest"
+        Write-Host "Updating version information for $path . . ."
 
-    $xml.Save($input_source)   
+        # load XML file
+        $xml = New-Object XML
+        $xml.Load($path)
 
-    Write-Host "Version number updated in manifest file" 
+        # select node and update version
+        $nodes = $xml.SelectNodes("/dotnetnuke/packages/package")
+        $nodes.SetAttribute("version", $input_version)
+
+        $xml.Save($path)   
+    }
+
+    Write-Host "Version number updated in all manifest file" 
 
     # Fail if any errors.
     # if ($failed) {
